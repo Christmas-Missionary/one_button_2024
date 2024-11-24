e065ecc00659624c964340269142531a655b054a
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-11-24 10:26:13 -0700

    all balls disappear if one hits player

--- src/bullet.gd
+++ src/bullet.gd
@@ -1,11 +1,14 @@
 extends CharacterBody2D
 class_name Bullet
 
+const GROUP_NAME: StringName = &"Bullets"
+
 func as_child_to(parent: Node) -> Bullet:
 	parent.add_child(self)
 	return self
 
 func spawn(player_pos: Vector2, rot: float) -> void:
+	add_to_group(GROUP_NAME)
 	var _direction: Vector2 = Vector2.DOWN.rotated(rot)
 	velocity = _direction * 480.0
 	position = player_pos + _direction * 25.0
