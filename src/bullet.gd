867f01998bc1502f0f99b80b69797411bb99e775
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-11-24 17:25:22 +0000

    bullet can bounce off of walls

--- src/bullet.gd
+++ src/bullet.gd
@@ -10,5 +10,13 @@ func spawn(player_pos: Vector2, rot: float) -> void:
 	velocity = _direction * 480.0
 	position = player_pos + _direction * 25.0
 
-func _physics_process(delta: float) -> void:
-	position += velocity * delta
+func _physics_process(_delta) -> void:
+	move_and_slide()
+	if is_on_wall():
+		match (get_wall_normal()):
+			Vector2.UP, Vector2.DOWN:
+				velocity.y = -velocity.y
+			Vector2.LEFT, Vector2.RIGHT:
+				velocity.x = -velocity.x
+			_:
+				velocity = velocity * (Vector2(-1, 1) if randi() % 2 else Vector2(1, -1))
