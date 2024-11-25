ceadd33d46423dc3a946c5304eddfc6cb7205503
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-11-25 08:35:09 -0700

    Node Renaming

--- src/camera_2d.gd
+++ src/camera_2d.gd
@@ -5,12 +5,11 @@ func _ready() -> void:
 
 func adjust_camera():
 	const INIT_SIZE: Vector2 = Vector2(650, 650)
-	var maze: = $/root/Main/TileMapLayer as Maze
+	var maze: = $/root/Main/Maze as Maze
 	var tile_size: int = maze.tile_set.tile_size.x # tile_size.y must be the same
 	var maze_size: Vector2 = Vector2(maze.x_dim + 2, maze.y_dim + 2) * tile_size
 	zoom = INIT_SIZE / maze_size
 	var center_cell: Vector2i = Vector2i(maze.x_dim, maze.y_dim) / 2
-	#center_cell.y -= 1
 	global_position = maze.to_global(maze.map_to_local(center_cell))
 
 func _process(delta: float) -> void:
