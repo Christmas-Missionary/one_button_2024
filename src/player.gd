cc2d8f27a64ea788ad69b3285264a54e7f2d4583
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-11-26 00:01:28 +0000

    group of bullets to bullet pool

--- src/player.gd
+++ src/player.gd
@@ -10,12 +10,13 @@ func _physics_process(delta: float) -> void:
 
 func _unhandled_key_input(event: InputEvent) -> void:
 	const _BULLET_SCENE: PackedScene = preload("res://src/bullet.tscn")
-	if event.is_action_pressed(&"OneButton") and Bullet.num_of_bullets < 5 and is_visible():
+	if event.is_action_pressed(&"OneButton") and _bullet_pool.get_child_count() < 5 and is_visible():
 		(_BULLET_SCENE.instantiate() as Bullet).as_child_to(_bullet_pool).spawn(position, rotation)
 		_shoot.play()
 
 func check_for_bullet(body: Node2D) -> void:
 	if body is Bullet:
-		get_tree().call_group(Bullet.GROUP_NAME, queue_free.get_method())
+		for bullet: Node in _bullet_pool.get_children():
+			bullet.queue_free()
 		hide()
 		_death.play()
