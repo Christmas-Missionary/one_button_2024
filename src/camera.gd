de29ee9e3bae9b0523e82b28944e5951d66cf01e
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-11-25 17:08:11 -0700

    inlined a bunch of stuff

--- src/camera.gd
+++ src/camera.gd
@@ -1,16 +1,7 @@
 extends Camera2D
 
+# tile_size.x and tile_size.y must be the same
 func _ready() -> void:
-	adjust_camera()
-
-func adjust_camera():
-	const INIT_SIZE: Vector2 = Vector2(650, 650)
-	var maze: = $/root/Main/Maze as Maze
-	var tile_size: int = maze.tile_set.tile_size.x # tile_size.y must be the same
-	var maze_size: Vector2 = Vector2(maze.x_dim + 2, maze.y_dim + 2) * tile_size
-	zoom = INIT_SIZE / maze_size
-	var center_cell: Vector2i = Vector2i(maze.x_dim, maze.y_dim) / 2
-	global_position = maze.to_global(maze.map_to_local(center_cell))
-
-func _process(delta: float) -> void:
-	adjust_camera()
+	var maze: = $/root/Main/Maze as Maze 
+	zoom = Vector2(650, 650) / (Vector2(maze.x_dim + 2, maze.y_dim + 2) * maze.tile_set.tile_size.x)
+	global_position = maze.to_global(maze.map_to_local(Vector2i(maze.x_dim, maze.y_dim) / 2))
