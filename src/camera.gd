1957a73be6faf2fe58f9268b43629bff6691f189
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-11-26 17:03:31 +0000

    level var has setter, uses signals

--- src/camera.gd
+++ src/camera.gd
@@ -1,7 +1,7 @@
 extends Camera2D
 
 # tile_size.x and tile_size.y must be the same
-func _ready() -> void:
-	var maze: = $/root/Main/Maze as Maze 
+func _adjust(level_val: int) -> void:
+	var maze: = $"../Maze" as Maze 
 	zoom = Vector2(650, 650) / (Vector2(maze.x_dim + 2, maze.y_dim + 2) * maze.tile_set.tile_size.x)
 	global_position = maze.to_global(maze.map_to_local(Vector2i(maze.x_dim, maze.y_dim) / 2))
