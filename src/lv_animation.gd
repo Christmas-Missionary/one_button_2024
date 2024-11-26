4238fe92dd3c2ea1ffffc17a4836139c650a2f9e
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-11-26 10:58:54 -0700

    node refs are onready vars

--- src/lv_animation.gd
+++ src/lv_animation.gd
@@ -1,12 +1,15 @@
 extends ColorRect
 
+@onready var _anime: = $Animation as AnimationPlayer
+@onready var _audio: = $Audio as AudioStreamPlayer
+@onready var _maze: = $"../Maze" as Maze
+@onready var _bullet_pool: Node = $/root/Main/BulletPool
+
 func _play() -> void:
-	($Animation as AnimationPlayer).play(&"Fade")
-	var _audio: = $Audio as AudioStreamPlayer
+	_anime.play(&"Fade")
 	_audio.play()
 	await _audio.finished
-	($"../Maze" as Maze).level += 1
+	_maze.level += 1
 	get_tree().set_pause(false)
-	var _bullet_pool: Node = $/root/Main/BulletPool
 	for bullet: Node in _bullet_pool.get_children():
 		bullet.queue_free()
