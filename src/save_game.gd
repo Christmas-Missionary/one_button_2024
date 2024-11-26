83032b21b0e6fc07c36524c288ae3cb00d94e465
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-11-26 16:13:43 -0700

    Save System

---
+++ src/save_game.gd
@@ -0,0 +1,29 @@
+extends Resource
+class_name SaveGame
+
+@export var level: int
+
+## These 2 save on file size, as opposed to one Array[Vector2i], not NOT as a .res file
+@export var x: Array[int]
+@export var y: Array[int]
+
+func save(level_: int, all_cells_: Array[Vector2i]) -> SaveGame:
+	level = level_
+	for i in all_cells_.size():
+		x.push_back(all_cells_[i].x)
+		y.push_back(all_cells_[i].y)
+	return self
+
+func to(save_path: String) -> void:
+	ResourceSaver.save(self, save_path)
+
+func are_cells_null() -> bool:
+	return x.size() != y.size()
+
+func get_cells() -> Array[Vector2i]:
+	if x.size() != y.size():
+		return []
+	var res: Array[Vector2i] = []
+	for i in x.size():
+		res.push_back(Vector2i(x[i], y[i]))
+	return res;
