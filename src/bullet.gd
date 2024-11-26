e7be497c6805b624d6a99f7d96e60cc12ae46051
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-11-26 00:14:00 +0000

    made things look nicer

--- src/bullet.gd
+++ src/bullet.gd
@@ -9,18 +9,19 @@ func as_child_to(parent: Node) -> Bullet:
 
 func spawn(player_pos: Vector2, rot: float) -> void:
 	var _direction: Vector2 = Vector2.DOWN.rotated(rot)
-	velocity = _direction * 480.0
+	velocity = _direction * 480.0 # 480.0 is speed
 	position = player_pos + _direction * 10.0
 
 func _physics_process(_delta) -> void:
 	move_and_slide()
-	if is_on_wall():
-		_bounce.play()
-		match (get_wall_normal()):
-			Vector2.UP, Vector2.DOWN:
-				velocity.y = -velocity.y
-			Vector2.LEFT, Vector2.RIGHT:
-				velocity.x = -velocity.x
-			_:
-				const RAND_ROT: float = TAU/12.0
-				velocity = (-velocity).rotated(randf_range(-RAND_ROT, RAND_ROT))
+	if !is_on_wall():
+		return
+	_bounce.play()
+	match (get_wall_normal()):
+		Vector2.UP, Vector2.DOWN:
+			velocity.y = -velocity.y
+		Vector2.LEFT, Vector2.RIGHT:
+			velocity.x = -velocity.x
+		_:
+			const RAND_ROT: float = TAU/12.0
+			velocity = (-velocity).rotated(randf_range(-RAND_ROT, RAND_ROT))
