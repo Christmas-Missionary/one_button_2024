d617dce669eb602406fb90648c1c3f2644f4be0c
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-11-27 03:02:52 +0000

    input cooldown

--- src/player.gd
+++ src/player.gd
@@ -3,18 +3,20 @@ extends Area2D
 @onready var _shoot: = $Shoot as AudioStreamPlayer
 @onready var _death: = $Death as AudioStreamPlayer
 @onready var _bullet_pool: Node = $/root/Main/BulletPool
+@onready var _cooldown: = $Cooldown as Timer
 
 func _physics_process(delta: float) -> void:
 	const _ROT_SPEED: float = PI / 1.2
 	rotate(_ROT_SPEED * delta)
 
 func _unhandled_key_input(event: InputEvent) -> void:
-	if event.is_action_pressed(&"OneButton") and _bullet_pool.get_child_count() < 5 and is_visible():
+	if event.is_action_pressed(&"OneButton") and _bullet_pool.get_child_count() < 5 and is_visible() and !_cooldown.time_left:
 		((preload("res://src/bullet.tscn")
 		.instantiate() as Bullet)
 		.as_child_to(_bullet_pool)
 		.spawn(position, rotation))
 		_shoot.play()
+		_cooldown.start()
 		set_process(true)
 
 const _RESET_TIME: float = 5.0
