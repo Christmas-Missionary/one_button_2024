b098463c8ff255bad648419039f14d2f5444b594
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-11-28 17:02:43 +0000

    pool functionality

--- src/lv_animation.gd
+++ src/lv_animation.gd
@@ -3,8 +3,6 @@ extends ColorRect
 @onready var _anime: = $Animation as AnimationPlayer
 @onready var _audio: = $Audio as AudioStreamPlayer
 @onready var _maze: = $"../Maze" as Maze
-@onready var _bullet_pool: Node = $/root/Main/BulletPool
-@onready var _particles_pool: Node = $/root/Main/ParticlesPool
 
 func _play() -> void:
 	_anime.play(&"Fade")
@@ -12,7 +10,5 @@ func _play() -> void:
 
 # Called by _anime just before finishing
 func _transition() -> void:
-	_maze.level += 1
 	get_tree().set_pause(false)
-	for node: Node in (_bullet_pool.get_children() + _particles_pool.get_children()):
-		node.queue_free()
+	_maze.level += 1
