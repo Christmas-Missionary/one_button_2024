88fa3af248194f1770b620e5b4787e4c8a96d68d
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2024-11-29 15:40:47 +0000

    start up screen revised for mobile and PC players

--- src/startup_timer.gd
+++ src/startup_timer.gd
@@ -1,8 +1,26 @@
 extends Timer
 
 func _ready() -> void:
-	start(randf_range(0.05, 2.0))
+	start(randf_range(0.8, 2.2))
+	set_process_unhandled_key_input(false)
 
-func change_scene() -> void:
-	await get_tree().create_timer(0.5).timeout
+func _await_player() -> void:
+	($"../Grid" as Sprite2D).hide()
+	if OS.has_feature("web_android") or OS.has_feature("web_ios"):
+		var touch: = ($"../TouchButton" as TouchScreenButton)
+		touch.show()
+		touch.pressed.connect(_start, CONNECT_ONE_SHOT)
+	else:
+		($"../Keyboard" as AnimatedSprite2D).show()
+		set_process_unhandled_key_input(true)
+
+func _unhandled_key_input(event: InputEvent) -> void:
+	if event.is_action_pressed(&"OneButton"):
+		set_process_unhandled_key_input(false)
+		_start()
+
+func _start() -> void:
+	($"../Keyboard/Anime" as AnimationPlayer).play(&"Fade Out")
+	($"../Audio" as AudioStreamPlayer).play()
+	await get_tree().create_timer(2.5).timeout
 	get_tree().change_scene_to_packed(preload("res://src/main.tscn"))
